5e0c2f71f6791292eb60d6c790bd4efdc570853c
Fabib <bo.fabian@protonmail.com>
2025-06-08 21:16:04 +0200

    V2 Started simple evaluation

--- Version2/evaluation.c
+++ Version2/evaluation.c
@@ -1,6 +1,13 @@
 #include "evaluation.h"
 #include "generation.h"
 
+#define VALUE_PAWN 100
+#define VALUE_KNIGHT 300
+#define VALUE_BISHOP 300
+#define VALUE_ROOK 500
+#define VALUE_QUEEN 900
+
+
 int evaluate_position(Board *board);
 
 int recursive_search_captures(Board *board, uint8_t depth, int alpha, int beta);
@@ -66,7 +73,30 @@ int recursive_search_captures(Board *board, uint8_t depth, int alpha, int beta) 
     return evaluate_position(board);
 }
 
+int evaluate_material(Board *board){
+    int evaluation = 0;
+    evaluation += __builtin_popcountl(board->pawn_W) * VALUE_PAWN;
+    evaluation += __builtin_popcountl(board->knight_W) * VALUE_KNIGHT;
+    evaluation += __builtin_popcountl(board->bishop_W) * VALUE_BISHOP;
+    evaluation += __builtin_popcountl(board->rook_W) * VALUE_ROOK;
+    evaluation += __builtin_popcountl(board->queen_W) * VALUE_QUEEN;
+
+    evaluation -= __builtin_popcountl(board->pawn_B) * VALUE_PAWN;
+    evaluation -= __builtin_popcountl(board->knight_B) * VALUE_KNIGHT;
+    evaluation -= __builtin_popcountl(board->bishop_B) * VALUE_BISHOP;
+    evaluation -= __builtin_popcountl(board->rook_B) * VALUE_ROOK;
+    evaluation -= __builtin_popcountl(board->queen_B) * VALUE_QUEEN;
+    return evaluation;
+}
+
+// Gibt einen Score für Weiß (Höher = besser für Weiß)
+int evaluate_position_for_white(Board *board) {
+    int evaluation = 0;
+    evaluation += evaluate_material(board);
+    return evaluation;
+}
+
+// Gibt einen Score für den Spieler, der gerade dran ist
 int evaluate_position(Board *board) {
-    //TODO
-    return 0;
+    return board->turn ? -evaluate_position_for_white(board) : evaluate_position_for_white(board);
 }
